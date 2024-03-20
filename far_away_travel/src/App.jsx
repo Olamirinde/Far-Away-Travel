4d3b6e698be84a8c8564ae2cdfd1c4542cca1f4c
Your Name <rindepixels@gmail.com>
2024-03-21 00:31:44 +0100

    Initial Commit

---
+++ far_away_travel/src/App.jsx
@@ -0,0 +1,15 @@
+import Form from "./Form";
+import PackingList from "./PackingList";
+import Stats from "./Stats";
+
+function App() {
+  return (
+    <>
+      <Form />
+      <PackingList />
+      <Stats />
+    </>
+  );
+}
+
+export default App;
